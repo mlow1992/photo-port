b1fe736f7e1cf5b0f3c18dfd973c50a09b88fec8
General Zelgius <mattlow123@gmail.com>
2022-05-15 11:44:07 -0400

    added front-end contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -22,6 +23,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}/>
         <About></About>
       </main>
